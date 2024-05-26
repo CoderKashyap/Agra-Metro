783f513019e5184221ec3106490e1eef1cba896c
CoderKashyap <kashyapking123456@gmail.com>
2024-05-27 02:50:25 +0530

    blog card done

--- frontend/src/components/home/LatestBlogs.tsx
+++ frontend/src/components/home/LatestBlogs.tsx
@@ -1,6 +1,7 @@
 
-// import second from '/promotionBanner/Holibanner.webp'
+import agraMetroGolLogo from '/metroLogoGol.png'
 import second from '/sunset.jpg'
+import "./index.css"
 
 
 export default function LatestBlogs() {
@@ -10,30 +11,147 @@ export default function LatestBlogs() {
         <>
 
 
-            <section className="xl:px-18 px-4 md:px-6 py-8">
+            <section className="font-quando xl:px-18 px-4 md:px-6 py-8">
 
-                <div className="font-quando lg:p-4 text-center p-2 text-gray-600 font-bold lg:text-3xl text-3xl">Latest Blogs</div>
+                <div className="lg:p-4 text-center p-2 text-gray-600 font-bold lg:text-3xl text-3xl">Latest Blogs</div>
 
                 <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 cursor-pointer items-center justify-items-center gap-6 py-6 px-3 md:px-1 w-full">
 
 
+
+
                     <div className="max-w-sm hover:shadow-md bg-white border border-gray-200 hover:border-gray-300 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                        <a href="#">
-                            <img className="rounded-t-lg" src={second} alt="" />
-                        </a>
-                        <div className="px-5 pb-4">
-                            <a >
-                                <h5 className="font-quando my-3 text-md sm:text-lg font-semibold tracking-tight text-gray-600 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-                            </a>
-                            {/* <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021.</p> */}
-                            <a className="inline-flex justify-center items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                                Read more
-                                <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
-                                </svg>
-                            </a>
+
+                        <img className="rounded-t-lg" src={second} alt="Blog thumbnail" />
+
+                        {/* Half  */}
+                        <div className="flex flex-row ">
+
+                            <div className='m-2'>
+                                <img className="rounded-full h- w-12" src={agraMetroGolLogo} alt="" />
+                            </div>
+
+                            <div className='flex flex-col gap-1 mr-2 my-3'>
+                                <h5 className="boNTXXBlogSection text-sm font-medium tracking-tight text-gray-600 dark:text-white">Note worthy techno dfg jifgh logy acquisitions 2021</h5>
+
+                                <div>
+                                    <p className="text-xs text-gray-500 dark:text-gray-400">Agra Metro</p>
+                                </div>
+
+                                <div className='flex justify-between items-center'>
+                                    <p className="text-xs text-gray-500 dark:text-gray-400">11/01/2000</p>
+
+                                    <a className="inline-flex justify-center items-center px-2 py-1.5 text-xs font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                        Read more
+                                        <svg className="rtl:rotate-180 w-3 h-3 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                                        </svg>
+                                    </a>
+                                </div>
+                            </div>
+
                         </div>
                     </div>
+
+                    <div className="max-w-sm hover:shadow-md bg-white border border-gray-200 hover:border-gray-300 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+
+                        <img className="rounded-t-lg" src={second} alt="Blog thumbnail" />
+
+                        {/* Half  */}
+                        <div className="flex flex-row ">
+
+                            <div className='m-2'>
+                                <img className="rounded-full h- w-12" src={agraMetroGolLogo} alt="" />
+                            </div>
+
+                            <div className='flex flex-col gap-1 mr-2 my-3'>
+                                <h5 className="boNTXXBlogSection text-sm font-semibold tracking-tight text-gray-600 dark:text-white">Note worthy techno dfg jifgh logy acquisitions 2021</h5>
+
+                                <div>
+                                    <p className="text-xs text-gray-500 dark:text-gray-400">Agra Metro</p>
+                                </div>
+
+                                <div className='flex justify-between items-center'>
+                                    <p className="text-xs text-gray-500 dark:text-gray-400">11/01/2000</p>
+
+                                    <a className="inline-flex justify-center items-center px-2 py-1.5 text-xs font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                        Read more
+                                        <svg className="rtl:rotate-180 w-3 h-3 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                                        </svg>
+                                    </a>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div className="max-w-sm hover:shadow-md bg-white border border-gray-200 hover:border-gray-300 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+
+                        <img className="rounded-t-lg" src={second} alt="Blog thumbnail" />
+
+                        {/* Half  */}
+                        <div className="flex flex-row ">
+
+                            <div className='m-2'>
+                                <img className="rounded-full h- w-12" src={agraMetroGolLogo} alt="" />
+                            </div>
+
+                            <div className='flex flex-col gap-1 mr-2 my-3'>
+                                <h5 className="boNTXXBlogSection text-sm font-semibold tracking-tight text-gray-600 dark:text-white">Note worthy techno dfg jifgh logy acquisitions 2021</h5>
+
+                                <div>
+                                    <p className="text-xs text-gray-500 dark:text-gray-400">Agra Metro</p>
+                                </div>
+
+                                <div className='flex justify-between items-center'>
+                                    <p className="text-xs text-gray-500 dark:text-gray-400">11/01/2000</p>
+
+                                    <a className="inline-flex justify-center items-center px-2 py-1.5 text-xs font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                        Read more
+                                        <svg className="rtl:rotate-180 w-3 h-3 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                                        </svg>
+                                    </a>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div className="max-w-sm hover:shadow-md bg-white border border-gray-200 hover:border-gray-300 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+
+                        <img className="rounded-t-lg" src={second} alt="Blog thumbnail" />
+
+                        {/* Half  */}
+                        <div className="flex flex-row ">
+
+                            <div className='m-2'>
+                                <img className="rounded-full h- w-12" src={agraMetroGolLogo} alt="" />
+                            </div>
+
+                            <div className='flex flex-col gap-1 mr-2 my-3'>
+                                <h5 className="boNTXXBlogSection text-sm font-semibold tracking-tight text-gray-600 dark:text-white">Note worthy techno dfg jifgh logy acquisitions 2021</h5>
+
+                                <div>
+                                    <p className="text-xs text-gray-500 dark:text-gray-400">Agra Metro</p>
+                                </div>
+
+                                <div className='flex justify-between items-center'>
+                                    <p className="text-xs text-gray-500 dark:text-gray-400">11/01/2000</p>
+
+                                    <a className="inline-flex justify-center items-center px-2 py-1.5 text-xs font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                        Read more
+                                        <svg className="rtl:rotate-180 w-3 h-3 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                                        </svg>
+                                    </a>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+
 
 
 
